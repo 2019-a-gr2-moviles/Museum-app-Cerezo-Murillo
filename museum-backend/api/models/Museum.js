98fa8c26825778b570b1f1039e1fd6e8a1266120
Jose7198 <jose719810@hotmail.com>
2019-06-23 16:13:20 -0500

    API models updated
    +Information model
    +Location model
    +Museum model
    +Person model
    +User model

---
+++ museum-backend/api/models/Museum.js
@@ -0,0 +1,28 @@
+/**
+ * Museum.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    name : {
+      type : 'string',
+      required : true,
+      minLength : 3,
+      maxLength : 64
+    },
+    museum_location_FK : {
+      model : 'location'
+    },
+    museum_information_FK : {
+      model : 'information'
+    }
+
+  },
+
+};
+
