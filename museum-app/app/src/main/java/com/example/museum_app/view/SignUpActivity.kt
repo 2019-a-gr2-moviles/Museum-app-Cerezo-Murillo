cc5b505bf0411ef1b1877edc662bf5d5623a89b9
JohaCR <caro.cerezor@gmail.com>
2019-07-31 12:18:08 -0500

    Sign up function

--- museum-app/app/src/main/java/com/example/museum_app/view/SignUpActivity.kt
+++ museum-app/app/src/main/java/com/example/museum_app/view/SignUpActivity.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.museum_app.R
+import com.example.museum_app.model.User
 import kotlinx.android.synthetic.main.activity_sign_up.*
+import kotlin.math.sign
 
 class SignUpActivity : AppCompatActivity() {
 
@@ -12,16 +14,44 @@ class SignUpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
         btn_singup.setOnClickListener {
-            goToMuseumActivity()
+            signUp()
+            Thread.sleep(100)
+            logIn()
         }
     }
 
     fun goToMuseumActivity(){
+
         val intentExplicito = Intent(
             this,
             MuseumView:: class.java
         )
         startActivity(intentExplicito)
+    }
+
+    fun signUp(){
+
+        var bodyPerson = listOf(
+            "name" to input_name.text,
+            "lastName" to input_last_name.text,
+            "emailAddress" to input_email.text
+        )
+
+        var bodyUser = listOf(
+            "userName" to input_user.text,
+            "password" to input_password.text
+        )
+
+
+    }
+
+    fun logIn(){
+        var newEmail = input_user.text.toString()
+        var newPass = input_password.text.toString()
+        if(User.login(newEmail,newPass) != null){
+            goToMuseumActivity()
+
+        }
     }
 
 }
